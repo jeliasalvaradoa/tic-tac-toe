2bcc4fc8ec129f888128e9ac3eb0addc61cfd1a0
jjaa <josealvarado30@hotmail.com>
2023-11-29 21:23:52 -0400

    agregando archivos

---
+++ src/components/logic/board.js
@@ -0,0 +1,22 @@
+import { WINNER_COMBOS } from "../../../constants.js"
+export const checkWinnerFrom = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && // 0--> X u O
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    //si no hay ganador
+    return null
+  }
+
+ export const checkEndGame = (newBoard) => {
+    //revísamos si hay un empate
+    // si no hay más espacios vacíos
+    // en el tablero
+    return newBoard.every((square) => square  != null)
+   }   
